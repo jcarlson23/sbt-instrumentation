b01788e029df45914488129911507a5026f4f63b
Jared Carlson <jared.carlson23@gmail.com>
2020-11-02 13:12:29 -0500

    Made a few changes so I can get external type information.

---
+++ analyses/external_types_plugin.cpp
@@ -0,0 +1,6 @@
+#include "external_types_plugin.hpp"
+#include <llvm/IR/LegacyPassManager.h>
+
+using namespace llvm;
+
+
---
+++ analyses/external_types_plugin.hpp
@@ -0,0 +1,43 @@
+#ifndef EXTERNAL_TYPES_PLUGIN_H
+#define EXTERNAL_TYPES_PLUGIN_H
+
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include <llvm/IR/Module.h>
+#include <llvm/Support/raw_ostream.h>
+#include "dg/llvm/ValueRelations/ValueRelations.h"
+#include "instr_plugin.hpp"
+
+namespace llvm {
+  class Module;
+  class Value;
+}
+
+class ExternalTypesPlugin : public InstrPlugin
+{
+
+
+  bool externalSuccess;
+  
+public:
+  ExternalTypesPlugin(llvm::Module* module) : InstrPlugin("External") {
+    externalSuccess = false;
+    // here we do our analysis of the module, looking for
+    // any external types.
+    
+  }
+  
+  bool supports(const std::string& query) override {
+    return query == "isExternalType";
+  }
+
+  std::string query(const std::string& query,
+		    const std::vector<llvm::Value *>& operands) {
+    return "Not Implemented";
+  }
+
+};
+
+#endif
