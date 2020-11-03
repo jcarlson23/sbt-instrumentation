9e40ef7cb597963cd696c047535bfa4738347a93
Jared Carlson <jared.carlson23@gmail.com>
2020-11-03 12:36:49 -0500

    Alright, got some of the hooks in, to grab function information, etc.  I just a bit more if I want to go down this route.

--- analyses/external_types_plugin.cpp
+++ analyses/external_types_plugin.cpp
@@ -6,8 +6,31 @@ using namespace llvm;
 bool ExternalTypesPlugin::processFunctions() {
   bool status = false;
   for (const llvm::Function &F : *mod) {
-    if ( F.hasName() ) {
+    if ( F.isDeclaration() ) {
 
+      const AttributeList &Attrs = F.getAttributes();
+      
+      SmallVector<std::pair<unsigned, MDNode*>, 4> MDs;
+      F.getAllMetadata(MDs);
+      // we can now print metadata, if we want..
+
+      auto linkage = F.getLinkage();
+
+      auto  visibility = F.getVisibility();
+      
+      // get the return Type of the function
+      llvm::Type * retType = F.getReturnType();
+
+      // get the function type
+      FunctionType* FT = F.getFunctionType();
+      
+      // get the argument information to the function
+      for (unsigned I=0, E=FT->getNumParams(); I != E; ++I) {
+	// from here we get the param type
+	llvm::Type * pType = FT->getParamType(I);
+	llvm::AttributeSet ArgAttrs = Attrs.getParamAttributes(I);
+	// this goes to TypePrinter
+      }
       
     }
   }
